5b74cb0e99b5de9c358c7230310b2039bd9f4f30
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2022-05-08 22:48:54 -0400

    redux devtools installed

--- redux/store.jsx
+++ redux/store.jsx
@@ -2,6 +2,6 @@ import { applyMiddleware, createStore } from 'redux';
 import rootReducer from './rootReducer';
 import thunk from 'redux-thunk';
 
-const store = createStore(applyMiddleware(thunk));
-
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const store = createStore(composeEnhancers(applyMiddleware(thunk)));
 export default store;
