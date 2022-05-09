75c418ba711d0778c0cb339d0e3cee0469163351
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2022-05-08 22:45:56 -0400

    redux skeleton setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,19 @@
-import Layout from "./components/layout";
-import SSRProvider from "react-bootstrap/SSRProvider";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "../styles/variables.scss";
-import "../styles/components/components.scss";
+import Layout from './components/layout';
+import SSRProvider from 'react-bootstrap/SSRProvider';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/variables.scss';
+import '../styles/components/components.scss';
+import { Provider } from 'react-redux';
+import store from '../redux/store';
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <SSRProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </SSRProvider>
+    <Provider store={store}>
+      <SSRProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SSRProvider>
+    </Provider>
   );
 }
---
+++ redux/rootReducer.js
@@ -0,0 +1,8 @@
+// import reducers here
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+  // reducers here ex: ''name of reducer': nameReducer
+});
+
+export default rootReducer;
