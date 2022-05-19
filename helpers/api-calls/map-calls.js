3204d0a0db941060b8b9cc14a5b454c026f32aaa
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2022-05-19 00:19:58 -0400

    fixed zip

--- helpers/api-calls/map-calls.js
+++ helpers/api-calls/map-calls.js
@@ -12,7 +12,9 @@ export const returnZip = async (lat, long) => {
       `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&key=${process.env.NEXT_PUBLIC_MAP_KEY}`
     )
     .then((res) => {
-      return res.data.results[4].address_components[0].long_name;
+      const addressComp = res.data.results[0].address_components;
+      return res.data.results[0].address_components[addressComp.length - 1]
+        .long_name;
     })
     .catch((err) => console.log(err));
   return zip;
