bc5c3279e30cab249e63b70467c389bfa1cd95b9
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2022-05-19 22:36:46 -0400

    please work

--- server/index.js
+++ server/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 const app = express();
-
 const cors = require('cors');
 app.use(cors());
 app.use(express.json());
