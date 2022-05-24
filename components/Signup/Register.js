a292cbaf4a5fa8a738b972f1057e543cd7ad9b3b
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2022-05-23 23:47:27 -0400

    mostly done

--- components/Signup/Register.js
+++ components/Signup/Register.js
@@ -1,42 +1,58 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGoogle, faFacebook } from '@fortawesome/free-brands-svg-icons';
 
 // use button, alert from our components
 // use input from react bootstrap
 
 const Signup = () => {
   return (
-    <div>
-      <div>Big Blue Box</div>
-      <Form>
-        <div>
-          <h2>Get Started.</h2>
-          <p>
-            Already have an account? <span>Log in</span>
-          </p>
-          <Button>Sign up with Google</Button>
-          <Button>Sign up with Facebook</Button>
-          <Form.Group className='mb-3' controlId='formBasicEmail'>
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type='email' placeholder='Enter email' />
-            <Form.Text className='text-muted'>
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
+    <div className='Signup'>
+      <div className='Signup--wrapper'>
+        <div></div>
+        <Form>
+          <div className='form--wrapper'>
+            <h2>Get Started.</h2>
 
-          <Form.Group className='mb-3' controlId='formBasicPassword'>
-            <Form.Label>Password</Form.Label>
-            <Form.Control type='password' placeholder='Password' />
-          </Form.Group>
-          <Form.Group className='mb-3' controlId='formBasicCheckbox'>
-            <Form.Check type='checkbox' label='Check me out' />
-          </Form.Group>
-          <Button variant='primary' type='submit'>
-            Submit
-          </Button>
-        </div>
-      </Form>
+            <p>
+              Already have an account? <span>Log in</span>
+            </p>
+            <div className='button--wrapper'>
+              <Button>
+                <FontAwesomeIcon icon={faGoogle} />
+                Sign up with Google
+              </Button>
+              <Button>
+                <FontAwesomeIcon icon={faFacebook} />
+                Sign up with Facebook
+              </Button>
+            </div>
+            <Form.Group className='mb-3' controlId='formBasicEmail'>
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type='email' placeholder='Enter email' />
+            </Form.Group>
+
+            <Form.Group className='mb-3' controlId='formBasicPassword'>
+              <Form.Label>Password</Form.Label>
+              <Form.Control type='password' placeholder='Password' />
+            </Form.Group>
+            <Form.Group className='mb-3' controlId='formBasicCheckbox'>
+              <div className='terms'>
+                <Form.Check type='checkbox' />
+                <label>
+                  I agree to Jettby's <span>Terms of Service</span> and{' '}
+                  <span>Privacy Policy</span>
+                </label>
+              </div>
+            </Form.Group>
+            <Button variant='primary' type='submit'>
+              Register
+            </Button>
+          </div>
+        </Form>
+      </div>
     </div>
   );
 };
