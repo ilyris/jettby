3229fa0323108bcd283dd1fe982958b64d8cd615
Dylan <dylan.sieren01@gmail.com>
2022-09-25 08:44:13 -0600

    Added dupe function back. Images now upload properly to cloudinary and delete properly, duplicates images dont get uploaded currently

--- components/inputs/FileInput/FileInput.tsx
+++ components/inputs/FileInput/FileInput.tsx
@@ -32,15 +32,35 @@ export default function FileInput({ label, type, cid, name }) {
   const [files, setFiles] = useState([]);
 
   // successfully uploaded images to cloudinary
-  const [cloudImgs, setCloudImgs] = useState([]);
+  const [cloud, setCloud] = useState([]);
+  let cloudImgs = [];
+
   const fileInput = useRef(null);
   const itemsRef = useRef([]);
 
+  const handleDupeImages = (arr) => {
+    const uniqueFile = new Set();
+    const unique = arr.filter((file) => {
+      const isDuplicate = uniqueFile.has(file.name);
+
+      uniqueFile.add(file.name);
+
+      if (!isDuplicate) {
+        return true;
+      }
+
+      return false;
+    });
+
+    return unique;
+  };
+
   const handleChange = async (e) => {
     // Set Files
-    if ([...e.target.files].length >= 20) {
+    if (handleDupeImages([...files, ...e.target.files]).length >= 20) {
       setAlertMessage("Exceeded maximum images of 20", "danger");
     }
+    setFiles(handleDupeImages([...files, ...e.target.files]));
 
     // Create Images
     let imagesSrcs = Array.from(e.target.files).map((file) => {
@@ -56,101 +76,87 @@ export default function FileInput({ label, type, cid, name }) {
     });
 
     let res = await Promise.all(imagesSrcs);
-
-    [...e.target.files].forEach(async (file, i) => {
-      // const sanitizedFileName = file.name.substring(
-      //   0,
-      //   file.name.lastIndexOf(".")
-      // );
-      setFiles([...files, ...e.target.files]);
-      setImages([...images, ...res]);
-
-      // if (cloudinaryImages.length) {
-      //   let flag = false;
-      //   cloudinaryImages.forEach((img) => {
-      //     console.log(sanitizedFileName === img.original_filename);
-      //     if (sanitizedFileName === img.original_filename) {
-      //       flag = true;
-      //       return;
-      //     }
-      //   });
-      //   if (flag) return;
-      // }
+    setImages(handleDupeImages([...images, ...res]));
+    console.log(e.target);
+    handleDupeImages([...files, ...e.target.files]).forEach(async (file, i) => {
+      const sanitizedFileName = file.name.substring(
+        0,
+        file.name.lastIndexOf(".")
+      );
+      if (cloud.length) {
+        let flag = false;
+        cloud.forEach((img) => {
+          if (sanitizedFileName === img.original_filename) {
+            flag = true;
+            return;
+          }
+        });
+        if (flag) return;
+      }
 
       const formData = new FormData();
       formData.append("file", file);
       formData.append("upload_preset", "Jettby");
 
-      const response = await axios.post(
-        process.env.NEXT_PUBLIC_CLOUDINARY_API,
-        formData,
-        {
-          onUploadProgress: () => {
-            if (itemsRef.current[i] == undefined) return;
-            if (!itemsRef.current[i].classList.contains("loaded")) {
-              itemsRef.current[i].classList.add("isLoading");
-            }
-          },
-        }
-      );
-      try {
-        // save photo data to local state and the url to the database
-        console.log(response.data);
-        console.log("setting state");
-        // setCloudImgs([response.data, ...cloudImgs]);
-        setCloudImgs([...cloudImgs, response.data]);
-        debugger;
-        console.log("done!");
-        itemsRef.current[i].classList.remove("isLoading");
-        itemsRef.current[i].classList.add("loaded");
-
-        dispatch(
-          setAlertMessage(
-            "Your images have been uploaded successfully!",
-            "success"
-          )
-        );
-      } catch (err) {
-        console.log(err);
-        dispatch(
-          setAlertMessage("Failed to upload images to server", "danger")
-        );
-      }
+      await axios
+        .post(
+          `${process.env.NEXT_PUBLIC_CLOUDINARY_API}/image/upload`,
+          formData,
+          {
+            onUploadProgress: () => {
+              // if you loop through only the recently added files, the index is reset back to 0
+              // targeting the incorrect itemRef
+              if (itemsRef.current[i] == undefined) return;
+              if (!itemsRef.current[i].classList.contains("loaded")) {
+                itemsRef.current[i].classList.add("isLoading");
+              }
+            },
+          }
+        )
+        .then((response) => {
+          cloudImgs.push(response.data);
+          setCloud([...cloud, ...cloudImgs]);
+          itemsRef.current[i].classList.remove("isLoading");
+          itemsRef.current[i].classList.add("loaded");
+          dispatch(
+            setAlertMessage(
+              "Your images have been uploaded successfully!",
+              "success"
+            )
+          );
+        })
+        .catch((err) => {
+          dispatch(
+            setAlertMessage("Failed to upload images to server", "danger")
+          );
+        });
     });
   };
 
   const handleDelete = (name) => {
-    console.log(name);
     const sanitizedName = name.substring(0, name.lastIndexOf("."));
 
     // filter files based on the image name
     const filteredFiles = images.filter((image) => {
       return image.name !== name;
     });
-
-    // filter cloudinary images based on if the file.name === cloudinaryImg.name
-    // const cloudinaryImgToDelete = cloudinaryImages.filter((img) => {
-    //   console.log(img.original_filename);
-    //   console.log(sanitizedName);
-    //   return img.original_filename === sanitizedName;
-    // });
-    console.log(cloudinaryImages);
-    return;
+    const deleteImg = cloud.filter(
+      (img) => img.original_filename === sanitizedName
+    );
 
     // api call to delete
     axios
-      .post(`https://api.cloudinary.com/v1_1/sieren/delete_by_token`, {
-        token: cloudinaryImgToDelete[0].delete_token,
+      .post(`${process.env.NEXT_PUBLIC_CLOUDINARY_API}/delete_by_token`, {
+        token: deleteImg[0].delete_token,
       })
       .then((res) => {
-        cloudinaryImages = [...cloudinaryImages].filter((img) => {
-          return img.original_filename !== sanitizedName;
-        });
+        setCloud(
+          [...cloud].filter((img) => img.original_filename !== sanitizedName)
+        );
         console.log(res);
       })
       .catch((err) => console.log(err));
 
-    console.log(filteredFiles);
     setFiles(filteredFiles);
     setImages(filteredFiles);
   };
